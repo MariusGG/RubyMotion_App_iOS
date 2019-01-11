8e607e95e9c36f5c130d9a4f0712a9ef757772ab
mariusgg <marius.g.guei@gmail.com>
2019-01-11 11:21:51 +0000

    added a note entity for notpad app

---
+++ app/note.rb
@@ -0,0 +1,9 @@
+class Note
+attr_accessor :title, :noteworthiness, :message
+
+    def initialize(note)
+      @title = note['title']
+      @noteworthiness = note['noteworthiness']
+      @message = note['message']
+    end
+end
